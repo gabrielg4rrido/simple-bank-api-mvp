ee49752948713dc7ce25d5604e6b10feff3fb1c8
gabrielgarrido <gabrielgarridoag@gmail.com>
2026-04-06 19:53:11 -0300

    feat: criando api de consulta de movimentações (histórico)

---
+++ src/main/java/br/com/cwi/bank/dto/AccountMovementResponse.java
@@ -0,0 +1,14 @@
+api depackage br.com.cwi.bank.dto;
+
+import br.com.cwi.bank.domain.MovementType;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record AccountMovementResponse(
+  Long id,
+  Long transferId,
+  MovementType movementType,
+  BigDecimal amount,
+  Instant createdAt
+) {}
--- src/main/java/br/com/cwi/bank/repository/AccountMovementRepository.java
+++ src/main/java/br/com/cwi/bank/repository/AccountMovementRepository.java
@@ -1,7 +1,10 @@
 package br.com.cwi.bank.repository;
 
 import br.com.cwi.bank.domain.AccountMovement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountMovementRepository extends JpaRepository<AccountMovement, Long> {
+  Page<AccountMovement> findByAccountId(Long accountId, Pageable pageable);
 }
