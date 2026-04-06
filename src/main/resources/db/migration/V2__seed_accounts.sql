c92ed4bed78c7627e26faf4ceec572c3f9e10091
gabrielgarrido <gabrielgarridoag@gmail.com>
2026-04-06 18:29:14 -0300

    chore: criando migrations inicias para criar tabela e popular (contas)

---
+++ src/main/resources/db/migration/V2__seed_accounts.sql
@@ -0,0 +1,3 @@
+insert into accounts (name, balance) values ('Alice', 1000.00);
+insert into accounts (name, balance) values ('Bob', 500.00);
+insert into accounts (name, balance) values ('Carol', 200.00);
